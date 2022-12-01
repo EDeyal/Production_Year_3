cb8ecae305a244a96f74dca536ef56e62aed3ea6
On <ontamam@gmail.com>
2022-12-01 16:17:45 +0200

    feel changes
    feels nice now

--- Production_Year_3_Project/Assets/Scripts/GroundCheck.cs
+++ Production_Year_3_Project/Assets/Scripts/GroundCheck.cs
@@ -1,10 +1,19 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
-
+using System;
+using UnityEngine.Events;
 public class GroundCheck : MonoBehaviour
 {
     [SerializeField] List<GroundCheckSensor> sensors = new List<GroundCheckSensor>();
     [SerializeField] LayerMask hitLayer;
+
+    public UnityEvent OnGrounded;
+    public UnityEvent OnNotGrounded;
+    private void Start()
+    {
+        StartCoroutine(WaitForGrounded());
+    }
     public bool IsAllGrounded()
     {
         foreach (var item in sensors)
@@ -41,13 +50,27 @@ public class GroundCheck : MonoBehaviour
         return false;
     }
 
-    private void OnDrawGizmosSelected()
+    IEnumerator WaitForGrounded()
+    {
+        yield return new WaitUntil(() => IsGrounded());
+        OnGrounded?.Invoke();
+        StartCoroutine(WaitForNotGrounded());
+    }
+    IEnumerator WaitForNotGrounded()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        OnNotGrounded?.Invoke();
+        StartCoroutine(WaitForGrounded());
+    }
+
+    private void OnDrawGizmos()
     {
         foreach (var item in sensors)
         {
             Vector3 relativePos = new Vector3(transform.position.x + item.Offset.x, transform.position.y + item.Offset.y);
             Vector3 to = item.Direcion * item.Range;
             to = new Vector3(relativePos.x + to.x, relativePos.y + to.y);
+            Gizmos.color = Color.red;
             if (IsSensorGrounded(item))
             {
                 Gizmos.color = Color.green;
--- Production_Year_3_Project/Assets/Scripts/Player Controler/CCController.cs
+++ Production_Year_3_Project/Assets/Scripts/Player Controler/CCController.cs
@@ -1,57 +1,162 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CCController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
     [SerializeField] float movementSpeed;
-    [SerializeField] float jumpForce;
-    [SerializeField] Vector3 velocity;
-    [SerializeField] GroundCheck groundCheck;
+    [SerializeField] float jumpHeight;
 
+    [SerializeField] Vector3 velocity;
+    [SerializeField] Vector3 gravity;
+
+    [SerializeField] GroundCheck groundCheck;
+    [SerializeField] GroundCheck ceilingDetector;
+
+    bool jumpPressed;
+    bool jumped;
+    bool coyoteAvailable;
+    [SerializeField] float coyoteThreshold;
+
+    [SerializeField] float apexWaitTime;
+    [SerializeField] float apexGravityScale;
 
     private float gravityForce = 9.81f;
     [SerializeField] float gravityScale;
+    private float startingGravityScale;
 
     List<Vector3> externalForces = new List<Vector3>();
-
+    public UnityEvent <Vector3>OnRecieveForce;
 
     private void Start()
     {
         InputManager.Instance.OnJumpDown.AddListener(Jump);
+        groundCheck.OnGrounded.AddListener(ResetVelocity);
+        groundCheck.OnNotGrounded.AddListener(StartCoyoteTime);
+        ceilingDetector.OnGrounded.AddListener(ResetVelocityY);
+        OnRecieveForce.AddListener(ApplyExtrenalForces);
+        startingGravityScale = gravityScale;
     }
 
+    private void Update()
+    {
+        SetInputVelocity();
+        ApplyGravity();
+
+    }
     private void LateUpdate()
     {
-        SetGravity();
-        SetVelocity();
+        MoveController();
     }
-
-    private void SetVelocity()
+    private void SetInputVelocity()
     {
-        velocity = new Vector3(InputManager.Instance.GetMoveVector().x * movementSpeed, 0, 0);
+        velocity.x = InputManager.Instance.GetMoveVector().x * movementSpeed;
 
-        controller.Move(velocity * Time.deltaTime);
+        if (jumpPressed)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravityForce);
+            jumped = true;
+            groundCheck.OnGrounded.AddListener(ResetJumped);
+            jumpPressed = false;
+            StartCoroutine(JumpApexWait());
+        }
     }
 
-
-    private void SetGravity()
+    private void ApplyGravity()
     {
         if (groundCheck.IsGrounded())
         {
-            return;
+            gravity = Vector3.zero;
         }
-
-        float gravity = gravityForce * gravityScale;
-        controller.Move(new Vector3(0, controller.velocity.y - gravity, 0) * Time.deltaTime);
+        else
+        {
+            gravity.y -= gravityForce * gravityScale * Time.deltaTime;
+        }
+        controller.Move(gravity * Time.deltaTime);
     }
+    private void MoveController()
+    {
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void ApplyExtrenalForces(Vector3 force)
+    {
+        Vector3 totalExternalForce = Vector3.zero;
+        foreach (var item in externalForces)
+        {
+            totalExternalForce += item;
+        }
+        externalForces.Clear();
+        controller.Move(totalExternalForce * Time.deltaTime);
+    }
+
     private void Jump()
     {
-        if (!groundCheck.IsGrounded())
+        if (groundCheck.IsGrounded() || coyoteAvailable)
         {
-            return;
+            jumpPressed = true;
         }
+    }
 
-        controller.Move(Vector3.up * jumpForce * Time.deltaTime);
+    public void ResetVelocity()
+    {
+        velocity = Vector3.zero;
+    }
+
+    public void ResetVelocityX()
+    {
+        velocity = new Vector2(0, velocity.y);
+
+    }
+    public void ResetVelocityY()
+    {
+        velocity = new Vector2(velocity.x, 0);
+    }
+    public void ResetVelocity(Vector3 givenVelocity)
+    {
+        velocity = givenVelocity;
+    }
+
+    private void ResetJumped()
+    {
+        jumped = false;
+        groundCheck.OnGrounded.RemoveListener(ResetJumped);
+    }
+
+    private void StartCoyoteTime()
+    {
+        if (!jumped) //if the character isnt jumping
+        {
+            StartCoroutine(CoyoteCounter());
+        }
+    }
+
+    IEnumerator CoyoteCounter()
+    {
+        coyoteAvailable = true;
+        yield return new WaitForSecondsRealtime(coyoteThreshold);
+        coyoteAvailable = false;
+    }
+
+    IEnumerator JumpApexWait()
+    {
+        yield return new WaitUntil(() => controller.velocity.y < 0);
+        gravityScale = apexGravityScale;
+        yield return new WaitForSecondsRealtime(apexWaitTime);
+        gravityScale = startingGravityScale;
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        externalForces.Add(force);
+        OnRecieveForce?.Invoke(force);
+    }
+
+    [ContextMenu("force")]
+    public void TestForce()
+    {
+        AddForce(Vector2.one * 5);
     }
 }
