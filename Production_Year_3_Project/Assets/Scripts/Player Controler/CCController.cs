2d52f652a9fdcd0c765bd98e5018e7249382d4db
On <ontamam@gmail.com>
2022-12-04 01:26:16 +0200

    jump works

--- Production_Year_3_Project/Assets/Scripts/Player Controler/CCController.cs
+++ Production_Year_3_Project/Assets/Scripts/Player Controler/CCController.cs
@@ -16,8 +16,14 @@ public class CCController : MonoBehaviour
     [SerializeField] GroundCheck ceilingDetector;
 
     bool jumpPressed;
-    bool jumped;
+    bool jumpIsHeld;
+
+    bool canHoldJump;
+    bool canJump;
+
     bool coyoteAvailable;
+    float jumpHeldTimer;
+    [SerializeField] float jumpHeldTime;
     [SerializeField] float coyoteThreshold;
 
     [SerializeField] float apexGravityScale;
@@ -25,20 +31,33 @@ public class CCController : MonoBehaviour
     private float gravityForce = 9.81f;
     [SerializeField] float gravityScale;
     private float startingGravityScale;
+    [SerializeField] float maxGravity;
 
     bool isFalling => DistanceFromPreviousPos().y < 0 && !groundCheck.IsGrounded();
     Vector3 oldPos;
 
     List<Vector3> externalForces = new List<Vector3>();
-    public UnityEvent <Vector3>OnRecieveForce;
+    public UnityEvent<Vector3> OnRecieveForce;
+    public UnityEvent OnJump;
 
 
     private void Start()
     {
+        //inputs
         InputManager.Instance.OnJumpDown.AddListener(Jump);
+        InputManager.Instance.OnJump.AddListener(HoldJump);
+        InputManager.Instance.OnJumpUp.AddListener(ReleaseJumpHeld);
+
+        //
         groundCheck.OnGrounded.AddListener(ResetVelocity);
+        groundCheck.OnGrounded.AddListener(ResetCanJump);
+        groundCheck.OnGrounded.AddListener(ResetCoyoteTime);
+        groundCheck.OnGrounded.AddListener(ResetCanHoldJump);
+        OnJump.AddListener(ResetJumpHeldTimer);
         groundCheck.OnNotGrounded.AddListener(StartCoyoteTime);
+
         ceilingDetector.OnGrounded.AddListener(ResetVelocityY);
+
         OnRecieveForce.AddListener(ApplyExtrenalForces);
         startingGravityScale = gravityScale;
     }
@@ -59,13 +78,21 @@ public class CCController : MonoBehaviour
 
         if (jumpPressed)
         {
+            Debug.Log("jumped");
             velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravityForce);
-            jumped = true;
-            groundCheck.OnGrounded.AddListener(ResetJumped);
             jumpPressed = false;
-            StartCoroutine(JumpApexWait());
+            OnJump?.Invoke();
+            //StartCoroutine(JumpApexWait());
+        }
+        else if (jumpIsHeld && jumpHeldTimer > 0)
+        {
+            Debug.Log("jump is held");
+            jumpHeldTimer -= Time.deltaTime;
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravityForce);
+            ResetGravity();
         }
     }
+
 
     private void ApplyGravity()
     {
@@ -77,6 +104,7 @@ public class CCController : MonoBehaviour
         {
             gravity.y -= gravityForce * gravityScale * Time.deltaTime;
         }
+        gravity.y = Mathf.Clamp(gravity.y, maxGravity * -1, 0);
         controller.Move(gravity * Time.deltaTime);
     }
     private void MoveController()
@@ -97,12 +125,46 @@ public class CCController : MonoBehaviour
 
     private void Jump()
     {
-        if (groundCheck.IsGrounded() || coyoteAvailable)
+        if (canJump && (groundCheck.IsGrounded() || coyoteAvailable))
         {
             jumpPressed = true;
+            canJump = false;
         }
     }
 
+    private void HoldJump()
+    {
+        if (!groundCheck.IsGrounded() && canHoldJump)
+        {
+            Debug.Log("jump was held");
+            canHoldJump = false;
+            jumpIsHeld = true;
+        }
+    }
+
+    private void ReleaseJumpHeld()
+    {
+        if (jumpIsHeld)
+        {
+            jumpIsHeld = false;
+        }
+        canHoldJump = false;
+    }
+
+    private void ResetJumpHeldTimer()
+    {
+        jumpHeldTimer = jumpHeldTime;
+    }
+
+    public void ResetGravity()
+    {
+        gravity = Vector3.zero;
+    }
+
+    private void ResetCoyoteTime()
+    {
+        coyoteAvailable = true;
+    }
     public void ResetVelocity()
     {
         velocity = Vector3.zero;
@@ -111,7 +173,6 @@ public class CCController : MonoBehaviour
     public void ResetVelocityX()
     {
         velocity = new Vector2(0, velocity.y);
-
     }
     public void ResetVelocityY()
     {
@@ -122,18 +183,30 @@ public class CCController : MonoBehaviour
         velocity = givenVelocity;
     }
 
-    private void ResetJumped()
+
+    private void ResetCanJump()
     {
-        jumped = false;
-        groundCheck.OnGrounded.RemoveListener(ResetJumped);
+        canJump = true;
     }
 
+    private void ResetCanHoldJump()
+    {
+        //canHoldJump = true;
+        StartCoroutine(WaitUntilJumpIsntHeld());
+    }
     private void StartCoyoteTime()
     {
-        if (!jumped) //if the character isnt jumping
+        if (canJump) //if the character isnt jumping
         {
             StartCoroutine(CoyoteCounter());
         }
+    }
+
+    IEnumerator WaitUntilJumpIsntHeld()
+    {
+        canHoldJump = false;
+        yield return new WaitUntil(() => !jumpIsHeld);
+        canHoldJump = true;
     }
 
     IEnumerator CoyoteCounter()
@@ -166,7 +239,7 @@ public class CCController : MonoBehaviour
         }
         Vector3 olderpos = oldPos;
         oldPos = transform.position;
-        return transform.position- olderpos;
+        return transform.position - olderpos;
     }
 
 }
