48a5a322a9bfb8dbb7161db2a69ee053ebea1973
On <ontamam@gmail.com>
2023-01-30 22:33:32 +0200

    player knockback fix because i broke it, colors

--- Production_Year_3_Project/Assets/Scripts/PlayerSwordVFX.cs
+++ Production_Year_3_Project/Assets/Scripts/PlayerSwordVFX.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerSwordVFX : MonoBehaviour
 {
     [SerializeField] private ParticleSystem quovaxDashParticle;
-
     public void PlayQuovaxDashParticle()
     {
         quovaxDashParticle.gameObject.SetActive(true);
