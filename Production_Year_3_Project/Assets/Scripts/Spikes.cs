35b14e0dca78e893b7e547782de103198aa50e4c
On <ontamam@gmail.com>
2023-02-06 02:39:11 +0200

    spikes layer  + lerp because tp didnt work, magic?

---
+++ Production_Year_3_Project/Assets/Scripts/Spikes.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : DamageDealingCollider
+{
+    [SerializeField] private Attack refAttack;
+    [SerializeField] private Transform respawnPoint;
+
+
+    private void Start()
+    {
+        OnColliderDealtDamage.AddListener(StartRespawnPlayer);
+        CacheReferences(refAttack);
+    }
+
+
+    private void StartRespawnPlayer()
+    {
+        //routine blackscreen fadeout...
+        //+ reset room
+        StartCoroutine(RespawnPlayer());
+    }
+
+    private IEnumerator RespawnPlayer()
+    {
+        GameManager.Instance.PlayerManager.PlayerController.ResetVelocity();
+        GameManager.Instance.PlayerManager.PlayerController.CanMove = false;
+        GameManager.Instance.PlayerManager.PlayerAbilityHandler.CanCast = false;
+        GameManager.Instance.PlayerManager.PlayerMeleeAttack.CanAttack = false;
+        Vector3 startPos = GameManager.Instance.PlayerManager.PlayerController.transform.position;
+        float counter = 0f;
+        while (counter < 1)
+        {
+            counter += Time.deltaTime;
+            GameManager.Instance.PlayerManager.PlayerController.transform.position = Vector3.Lerp(startPos, respawnPoint.position, counter);
+            yield return new WaitForEndOfFrame();
+        }
+        //GameManager.Instance.PlayerManager.PlayerController.transform.position = new Vector3(respawnPoint.position.x, respawnPoint.position.y, respawnPoint.position.z);
+        yield return new WaitForEndOfFrame();
+        GameManager.Instance.PlayerManager.PlayerController.CanMove = true;
+        GameManager.Instance.PlayerManager.PlayerAbilityHandler.CanCast = true;
+        GameManager.Instance.PlayerManager.PlayerMeleeAttack.CanAttack = true;
+
+
+    }
+}
