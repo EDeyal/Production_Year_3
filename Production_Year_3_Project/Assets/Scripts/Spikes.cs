230d30bb462477f5af5d55f7222c19ffb0c547e5
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-02-06 13:18:32 +0200

    Lior Spikes Fix
    * TODO: Move player should be a function in player, that should also reset all forces applied to the player CC
    * Changed player location in On Scene

--- Production_Year_3_Project/Assets/Scripts/Spikes.cs
+++ Production_Year_3_Project/Assets/Scripts/Spikes.cs
@@ -27,14 +27,19 @@ public class Spikes : DamageDealingCollider
         GameManager.Instance.PlayerManager.LockPlayer();
         yield return StartCoroutine(GameManager.Instance.UiManager.PlayerHud.FadeToBlack());
         Vector3 startPos = GameManager.Instance.PlayerManager.PlayerController.transform.position;
-        float counter = 0f;
-        while (counter < 1)
-        {
-            counter += Time.deltaTime;
-            GameManager.Instance.PlayerManager.PlayerController.transform.position = Vector3.Lerp(startPos, respawnPoint.position, counter);
-            yield return new WaitForEndOfFrame();
-        }
-        //GameManager.Instance.PlayerManager.PlayerController.transform.position = new Vector3(respawnPoint.position.x, respawnPoint.position.y, respawnPoint.position.z);
+        //float counter = 0f;
+        //while (counter < 1)
+        //{
+        //    counter += Time.deltaTime;
+        //    GameManager.Instance.PlayerManager.PlayerController.transform.position = Vector3.Lerp(startPos, respawnPoint.position, counter);
+        //    yield return new WaitForEndOfFrame();
+        //}
+
+        // TODO: Move player should be a function in player, that should also reset all forces applied to the player CC
+        yield return new WaitForEndOfFrame();
+        GameManager.Instance.PlayerManager.PlayerController.enabled = false;
+        GameManager.Instance.PlayerManager.PlayerController.transform.position = new Vector3(respawnPoint.position.x, respawnPoint.position.y, respawnPoint.position.z);
+        GameManager.Instance.PlayerManager.PlayerController.enabled = true;
         GameManager.Instance.InputManager.LockInputs = true;
         GameManager.Instance.PlayerManager.UnLockPlayer();
         yield return StartCoroutine(GameManager.Instance.UiManager.PlayerHud.FadeFromBlack());
