e621dcef0fb851132da1e4afa9ae511c8e145faf
EDeyal <75429849+EDeyal@users.noreply.github.com>
2023-07-14 19:50:14 +0300

    ProgressionParticle

--- Production_Year_3_Project/Assets/Scripts/PowerUp.cs
+++ Production_Year_3_Project/Assets/Scripts/PowerUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-
+    [SerializeField] protected Color _progressionColor;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
